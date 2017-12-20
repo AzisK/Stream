8865450f899b1e71fa12f8bd82cda9ff2114dfc6
AzisK <azuolas.krusna@yahoo.com>
2017-12-20 19:52:14 +0200

    Add pandas and display the dataframe of music

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, send_file, Response, stream_with_context
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
+import pandas as pd
 from io import BytesIO
 import os
 
 app = Flask(__name__)
+DATABASE_URI = 'postgres+psycopg2://mntuzyvzcgjlsu:82272757136c01021ad21469e052f8ad43df98927ccc23c8dccc200447bcc98a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/d713cmvmuhr4jd'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://:postgres@localhost/postgres'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://mntuzyvzcgjlsu:82272757136c01021ad21469e052f8ad43df98927ccc23c8dccc200447bcc98a@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/d713cmvmuhr4jd'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 db = SQLAlchemy(app)
 
 class Music(db.Model):
@@ -70,6 +72,15 @@ def dislikeAdd():
     q = 'UPDATE music SET dislikes = dislikes + 1 WHERE id = {}'.format(id)
     db.engine.execute(q)
     return id
+
+@app.route('/df')
+def df():
+    conn = psycopg2.connect("dbname='d713cmvmuhr4jd' user='mntuzyvzcgjlsu' host='ec2-54-217-250-0.eu-west-1.compute.amazonaws.com' password='82272757136c01021ad21469e052f8ad43df98927ccc23c8dccc200447bcc98a'")
+    q = "SELECT id, name, numplays, numlikes, dislikes FROM music"
+    df = pd.io.sql.read_sql_query(q, conn)
+    df.set_index(['id'], inplace=True)
+    df.index.name=None
+    return render_template('view.html', tables=[df.to_html(classes='df')], titles = ['na', 'Dataframe'])
 
 if __name__ == '__main__':
     app.run(debug=True)
